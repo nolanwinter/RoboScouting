7e5261cec099ad679b3e3ac97c6a948d5e58c6f7
nolanwinter <nolanwinter@users.noreply.github.com>
2024-02-22 21:50:44 -0500

    Small Tweaks and Bugfixes
    - Info screen is done now, for real.

--- RoboScout2024/info_page.lua
+++ RoboScout2024/info_page.lua
@@ -55,14 +55,19 @@ function scene:create(event)
     link_1:setFillColor(0.2,0.2,0.2)
     local link_1_line = display.newLine( sceneGroup, link_1.x - (link_1.width/2), link_1.y + link_1.height, link_1.x + (link_1.width/2), link_1.y + link_1.height)
     link_1_line:setStrokeColor(0.2,0.2,0.2)
+    link_1_line.strokeWidth=1
     local info_2 = display.newText({parent=sceneGroup, text=info_str_2, x=display.contentCenterX,y=link_1.y+link_1.height, font=native.systemFont, fontSize=15, align="center"})
     info_2.anchorY=0
     info_2:setFillColor(0,0,0)
     local link_2 = display.newText({parent=sceneGroup, text=link_str_2..".", x=display.contentCenterX,y=info_2.y+info_2.height, font=native.systemFont, fontSize=15, align="center"})
     link_2.anchorY=0
     link_2:setFillColor(0.2,0.2,0.2)
-    local link_2_line = display.newLine( sceneGroup, link_2.x - (link_2.width/2), link_2.y + link_2.height, link_2.x + (link_2.width/2), link_2.y + link_2.height)
-    link_2_line:setStrokeColor(0.2,0.2,0.2)
+    local link_2_line_upper = display.newLine(sceneGroup, link_2.x - (link_2.width/2)+27, link_2.y + (link_2.height/2), link_2.x + (link_2.width/2)-27, link_2.y + (link_2.height/2))
+    link_2_line_upper:setStrokeColor(0.2,0.2,0.2)
+    link_2_line_upper.strokeWidth=1
+    local link_2_line_lower = display.newLine(sceneGroup, link_2.x - (link_2.width/2), link_2.y + link_2.height, link_2.x + (link_2.width/2), link_2.y + link_2.height)
+    link_2_line_lower:setStrokeColor(0.2,0.2,0.2)
+    link_2_line_lower.strokeWidth=1
     link_1:addEventListener("tap", function() system.openURL(link_str_1) end)
     link_2:addEventListener("tap", function() system.openURL(link_str_2) end)
 end
--- RoboScout2024/main.lua
+++ RoboScout2024/main.lua
@@ -9,5 +9,3 @@ Data.read_history()
 --composer.gotoScene("test")
 composer.gotoScene("match_info")
 -- convert to byte, subtract 40 so that they are all double digits, if less than 10, add a leading zero
-
---properly attribute the qr generation by https://github.com/speedata/luaqrcode/blob/master/qrencode.lua
